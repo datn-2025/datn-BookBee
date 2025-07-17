634fad687df90716327ab4a172fcb44f4f7fc526
Đặng Duy Tiến <tienddph50842@gmail.com>
2025-07-17 20:29:00 +0700

    hiển thị số lượng cart ở nav

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -132,7 +132,11 @@
                         <svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"/>
                         </svg>
-                        <span class="absolute -top-1 -right-1 h-4 w-4 bg-black text-white text-xs rounded-full flex items-center justify-center">0</span>
+                        @if(isset($cartItemCount) && $cartItemCount > 0)
+                            <span class="absolute -top-1 -right-1 h-4 w-4 bg-black text-white text-xs rounded-full flex items-center justify-center">
+                                {{ $cartItemCount > 99 ? '99+' : $cartItemCount }}
+                            </span>
+                        @endif
                     </a>
                 </div>
             </div>
