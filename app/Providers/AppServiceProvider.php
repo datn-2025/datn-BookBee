5a5362518bdc85fc31623cec81db1e37db007707
Đặng Duy Tiến <tienddph50842@gmail.com>
2025-07-17 20:28:41 +0700

    hiển thị số lượng cart ở nav

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 use App\Models\Payment;
 use App\Observers\PaymentObserver;
+use App\Http\ViewComposers\CartComposer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,5 +24,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Payment::observe(PaymentObserver::class);
+        
+        // Share cart count with navbar and other views that need it
+        View::composer([
+            'layouts.partials.navbar',
+            'layouts.app'
+        ], CartComposer::class);
     }
 }
