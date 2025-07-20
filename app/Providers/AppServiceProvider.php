ff04dd79fa9dc4cf9eaaa4bb6be6aeb7d867cbd5
vanvan9825 <tvan19525@gmail.com>
2025-07-20 18:28:23 +0700

    `Added broadcasting middleware and routes, updated timezone, removed migration, added chat functionality, and updated backend layout`

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Models\Payment;
 use App\Observers\PaymentObserver;
+use Laravel\Sanctum\PersonalAccessToken;
+use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,5 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Payment::observe(PaymentObserver::class);
+        // Register the PersonalAccessToken model for Sanctum
+        Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
     }
 }
