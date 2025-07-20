d245d8d1560d0ae8f3d81b0504435b46c5ded82f
vanvan9825 <tvan19525@gmail.com>
2025-07-20 18:30:46 +0700

    `Added notification div, chat widget, and chat script to layouts, and added chat real-time routes.`

---
+++ resources/js/bootstrap.js
@@ -0,0 +1,11 @@
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allow your team to quickly build robust real-time web applications.
+ */
+
+import axios from 'axios';
+window.axios = axios;
+
+// Echo configuration is now handled in echo.js
+import './echo';
