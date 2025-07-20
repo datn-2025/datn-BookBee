32708f6362b82cb009c3fe3a32cb2f4f4686a9b1
vanvan9825 <tvan19525@gmail.com>
2025-07-20 18:32:18 +0700

    `Added broadcast and chat real-time routes to web.php`

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ConversationController;
+use App\Http\Controllers\Api\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::apiResource('users', UserController::class);
+// ->middleware('auth:sanctum'); // Ensure that the UserController is protected by Sanctum authentication
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/messages', [ConversationController::class, 'index']);
+    Route::post('/messages', [ConversationController::class, 'store']);
+    Route::delete('/messages/{id}', [ConversationController::class, 'destroy']);
+});
