40fc7f2e62734d88f7007ee4ea06462fd708bf0f
vanvan9825 <tvan19525@gmail.com>
2025-07-25 19:06:22 +0700

    `Added chat functionality with real-time updates, conversation list, and user information display`

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,11 +1,16 @@
 /**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allow your team to quickly build robust real-time web applications.
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
 import axios from 'axios';
 window.axios = axios;
 
-// Echo configuration is now handled in echo.js
-import './echo';
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * Echo configuration is handled in echo.js file
+ * Do not configure Echo here to avoid conflicts
+ */
+
