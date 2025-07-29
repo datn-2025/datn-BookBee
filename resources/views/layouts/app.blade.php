29d0c58bbddf2cf50ee6b3a662ac353e5ec8682f
nguyen doan hoang <nguyendoanhoang4@gmail.com>
2025-07-29 15:55:44 +0700

    Cập nhật layout app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>{{ get_setting() ? get_setting()->name_website : 'BookBee' }} - @yield('title')</title>
     <link rel="shortcut icon" href="{{ asset('storage/' . (get_setting() ? get_setting()->favicon : 'default_favicon.ico')) }}" />
-
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
    
 
     <!-- Bootstrap CSS -->
