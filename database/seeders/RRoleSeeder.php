13a148a429400d53fce1edd9969390bad7734427
nguyen doan hoang <nguyendoanhoang4@gmail.com>
2025-07-30 13:40:28 +0700

    Thêm dữ liệu mẫu cho vai trò

--- database/seeders/RRoleSeeder.php
+++ database/seeders/RRoleSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Permission;
 
 use App\Models\Role;
 use Illuminate\Database\Seeder;
@@ -15,16 +16,46 @@ class RRoleSeeder extends Seeder
     public function run()
     {
         // Add role entries
-        $roles = [
-            ['name' => 'Admin'],
-            ['name' => 'User'],
-            ['name' => 'Staff'],
-            // You can add more roles here as required
+          $roles = [
+            [
+                'name' => 'Admin',
+                'description' => 'Quản trị viên hệ thống'
+            ],
+            [
+                'name' => 'User',
+                'description' => 'Người dùng thông thường'
+            ],
+            [
+                'name' => 'Staff',
+                'description' => 'Nhân viên quản lý'
+            ]
         ];
 
-        // Loop through each role and create a new record in the roles table
-        foreach ($roles as $role) {
-            Role::create($role);    
+        foreach ($roles as $roleData) {
+            // Tạo role
+            $role = Role::create($roleData);
+
+            // Gán quyền nếu cần
+            if ($role->name === 'Admin') {
+                $role->permissions()->attach(Permission::all());
+            }
+
+            if ($role->name === 'Staff') {
+                $restrictedPermissions = [
+                    'users.manage',
+                    'users.force-delete',
+                    'roles.manage',
+                    'payment-methods.create',
+                    'payment-methods.edit',
+                    'payment-methods.delete',
+                    'attributes.create',
+                    'attributes.edit',
+                    'attributes.delete'
+                ];
+
+                $allowedPermissions = Permission::whereNotIn('slug', $restrictedPermissions)->get();
+                $role->permissions()->attach($allowedPermissions);
+            }
         }
-    }
+}
 }
